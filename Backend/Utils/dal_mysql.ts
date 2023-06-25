9daa3a3848eb7436a293e47f43830dbcf706d703
Daniel Gapso <dandan11110000@gmail.com>
2023-06-25 16:42:49 +0300

    ⚒️

--- Backend/Utils/dal_mysql.ts
+++ Backend/Utils/dal_mysql.ts
@@ -2,25 +2,23 @@ import mysql from "mysql";
 import config from "./Config";
 
 const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST || config.mySQLhost,
-    user: process.env.MYSQL_USER || config.mySQLuser,
-    password: process.env.MYSQL_PASSWORD || config.mySQLpass,
-    database: process.env.MYSQL_DATABASE || config.mySQLdatabase,
+  host: config.mySQLhost,
+  user: config.mySQLuser,
+  password: config.mySQLpass,
+  database: config.mySQLdatabase,
   });
   
-
-  const execute = (sql: string, params?: any): Promise<any> => {
+  const execute = (sql: string , params?: any): Promise<any> => {
     return new Promise<any>((resolve, reject) => {
-      connection.query(sql, params, (err, res) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(res);
-      });
+        connection.query(sql, (err, res) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(res);
+        });
     });
-  };
-  
+}
 
 export default {
     execute
